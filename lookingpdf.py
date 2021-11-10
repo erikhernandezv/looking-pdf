3aae3a318762c78ce54940f2d35af41a5585da0e
Erik Hernandez V <erikdhv@gmail.com>
2021-11-10 15:10:00 -0500

    Update lookingpdf.py

--- lookingpdf.py
+++ lookingpdf.py
@@ -1,1 +1,7 @@
+import pdfplumber
 
+with pdfplumber.open("JHDSS_CourseDependencies.pdf") as pdf:
+    first_page = pdf.pages[0]
+    print(first_page.chars[0])
+    print("\n")
+    print(first_page.extract_texts())
